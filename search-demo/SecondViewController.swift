db19013acaabe0f30e0c716967197848eacb71a7
Brian Clubb <bclubb@gmail.com>
2015-07-31 13:07:08 -0500

    Initial Commit

---
+++ search-demo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  search-demo
+//
+//  Created by Brian Clubb on 7/31/15.
+//  Copyright © 2015 Bubblesort Laboratories LLC. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
